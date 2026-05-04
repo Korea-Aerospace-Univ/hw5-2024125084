876fd7395cb418648613ccd15ffd08e6c0f3db9b
2024125084 <kmj35003500@gmail.com>
2026-05-04 15:48:24 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -11,7 +11,7 @@ int main() {
             for (int c = 1; c <= N / 200; c++) {                    
                 if (900 * a + 750 * b + 200 * c == N) {             
                     if (c < a || c < b) {                             
-                        printf("%d %d %d\n", a, b, c);            
+                        printf(%d %d %d\n", a, b, c);            
                         found = 1;                                     
                     }                                                
                 }                                                      
